869c0a0cb5b6e0d95fa11f9ff6fceafd1bce1311
Colin Woodbury <colin@fosskers.ca>
2022-11-26 05:33:41 +0900

    Fix warnings and modernize (#1)
    * fix: native Emacs Lisp warnings
    
    * fix: remove obsolete `cl` import
    
    * docs: add expected fields to top-level module docs
    
    * fix: follow all advice from `package-lint`

--- ob-dart.el
+++ ob-dart.el
@@ -1,23 +1,19 @@
-;;; ob-dart.el --- org-babel functions for Dart evaluation
+;;; ob-dart.el --- Evaluate Dart source blocks in org-mode -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2016 Free Software Foundation, Inc.
 
 ;; Author: Milan Zimmermann
-;; Keywords: literate programming, reproducible research, emacs, org, babel, dart
+;; Maintainer: Milan Zimmermann
+;; Created: July 7, 2016
+;; Modified: May 19, 2022
+;; Version: 1.0.1
+;; Keywords: languages
 ;; Homepage: http://github.org/mzimmerm/ob-dart
-
-;; This is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; For the GNU General Public License, see <http://www.gnu.org/licenses/>.
-
+;; Package-Requires: ((emacs "24.4"))
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;;
+;; This file is not part of GNU Emacs.
+;;
 ;;; Commentary:
 ;;    - Currently only supports execution of Dart code that can be wrapped
 ;;      in Dart main() method body.
@@ -25,13 +21,13 @@
 ;;    - Support any valid Dart code, including class definitions and
 ;;      the main() method
 ;;    - Session support
-
+;;
 ;;; Requirements:
 ;; - Dart language installed - An implementation can be downloaded from
 ;;                             https://www.dartlang.org/downloads/
-;; - The dart executable is on the PATH 
-;; - (Optional) Dart major mode -  Can be installed from MELPA
-
+;; - The dart executable is on the PATH
+;; - (Optional) Dart major mode from MELPA
+;;
 ;; Notes:
 ;;   - Code follows / inspired by these previously supported org-languages,
 ;;     roughly in this order:
@@ -40,38 +36,34 @@
 ;;     - ob-groovy.el
 ;;     - ob-R.el
 ;;     - ob-python.el
-
+;;
 ;;; Code:
 
 (require 'ob)
-(eval-when-compile (require 'cl))
 
 (defvar org-babel-tangle-lang-exts)
 (add-to-list 'org-babel-tangle-lang-exts '("dart" . "dart"))
 (defvar org-babel-default-header-args:dart '())
-(defvar org-babel-dart-command "dart"
-  "Name of the command to use for executing Dart code. todo - Windows")
+(defvar ob-dart-command "dart"
+  "Name of the command to use for executing Dart code.
+Windows support is pending.")
 
 (defun org-babel-execute:dart (body params)
-  "
-Execute a block of Dart code with org-babel.  This function is
-called by `org-babel-execute-src-block'
+  "Execute a block of Dart code with org-babel.
+This function is called by `org-babel-execute-src-block'.
 
 Args:
   BODY   - String - Dart code from org file, between #+begin_src and #+end_src
-                  - should be named: dart-src
-  PARAMS - List   - Org parameters after #+begin_src and #+end_src
-                  - todo - document better, is this correct?
-"
+  PARAMS - List   - Org Babel parameters after #+begin_src"
   (message "executing Dart source code block")
   (let* ((processed-params (org-babel-process-params params))
-         (session (org-babel-dart-initiate-session (nth 0 processed-params)))
-         (vars (nth 1 processed-params))
+         (session (ob-dart-initiate-session (nth 0 processed-params)))
+         ;; (vars (nth 1 processed-params))  UNUSED?
          (result-params (nth 2 processed-params))
          (result-type (cdr (assoc :result-type params)))
          (full-body (org-babel-expand-body:generic
                      body params))
-         (result (org-babel-dart-evaluate
+         (result (ob-dart-evaluate
                   session full-body result-type result-params)))
 
     (org-babel-reassemble-table
@@ -82,19 +74,20 @@ Args:
       (cdr (assoc :rowname-names params)) (cdr (assoc :rownames params))))))
 
 
-(defun org-babel-dart-table-or-string (results)
-  "
-Convert RESULTS into an appropriate elisp value.
+(defun ob-dart-table-or-string (results)
+  "Convert RESULTS into an appropriate elisp value.
 
-The default core implementation org-babel-script-escape behaves as follows:
-  - If RESULTS look like a table (grouped using () or {} or [] and delimited by comme), 
-     then convert them into an Emacs-lisp table (list of lists), 
-  - otherwise return the results unchanged as a string.
+The default core implementation `org-babel-script-escape' behaves as follows:
+
+If RESULTS look like a table (grouped using () or {} or [] and
+delimited by commas), then convert them into an Emacs-lisp
+table (list of lists),
+
+Otherwise, return the results unchanged as a string.
 
 Args:
   RESULTS - String - String resulting from Dart invocation, and printed to stdio
-                     by stdout.write() or print()
-"
+                     by stdout.write() or print()"
   (org-babel-script-escape results))
 
 ;; Variable which returns Dart code in String.
@@ -103,20 +96,19 @@ Args:
 ;;   in a Dart main() method. There are some added runZoned() tricks to either use
 ;;   the printed strings or returned value as results of the #+begin_src and #+end_src
 ;;   code.
-;; 
+;;
 ;; The above behaviour is controlled by an argument passed from elisp
 ;;   to the during 'org-babel-eval dart-code argument'.
 ;;
 ;; In particular, if the passed argument (named results_collection_type in code) is:
 ;;   - "output":
 ;;     - Strings from Dart print() is send to the standart output,
-;;       and becomes the result 
+;;       and becomes the result
 ;;   - "value":
 ;;     - Value of the last statement converted to String  is send to the standart output,
 ;;       and becomes the result.
 
-(defvar org-babel-dart-wrapper-method
-
+(defvar ob-dart-wrapper-method
   "
 //import 'dart:analysis_server';
 //import 'dart:analyzer';
@@ -146,7 +138,7 @@ import 'dart:mirrors';
 ////   https://gist.github.com/0e1dd60ca06369f7990d0ecfda8ed6a7
 ////   https://dartpad.dartlang.org/0e1dd60ca06369f7990d0ecfda8ed6a7
 class Gen {
-   
+
   // Wrapped code from the org file, between #+begin_src and #+end_src
 
   runSrc() {
@@ -156,12 +148,12 @@ class Gen {
     //   See (format org-babel-dart-wrapper-method body)
     %s
   }
-  
+
   // run, allow print to stdout, and  ignore return value
   runSrcResultsOutput() {
     runSrc();
   }
-  
+
   // run, ignore print to stdout, and use return value  (which will be printed to stdout)
   runSrcResultsValue() {
     // ignore prints to stdout
@@ -172,41 +164,37 @@ class Gen {
     	print: (self, parent, zone, message) {
       	// Ignore argument message passed to print.
     	}));
-    
+
     return retval;
   }
 }
 
 void main(List args) {
-  
-  // new Gen().runSrcResultsOutput();     
+
+  // new Gen().runSrcResultsOutput();
   // print(\"${new Gen().runSrcResultsValue()}\");
 
   var results_collection_type = null;
   if (args != null && args.length > 0) {
     results_collection_type = args.elementAt(0);
   }
-  
+
   if (results_collection_type == \"output\") {
   	// generate this for :results output
   	new Gen().runSrcResultsOutput();
-  } else if (results_collection_type == \"value\") {   
-  	// generate this for :results value  (use return value and print it) 
+  } else if (results_collection_type == \"value\") {
+  	// generate this for :results value  (use return value and print it)
   	// this works because in Dart print inside print still goes to stdout
-  	stdout.write(\"${new Gen().runSrcResultsValue()}\"); // print with no newline. Needed for 
+  	stdout.write(\"${new Gen().runSrcResultsValue()}\"); // print with no newline. Needed for
   } else {
     throw new Exception(\"Invalid collection type in results: ${results_collection_type}. Only one of [output/value] allowed.\");
   }
-  
+
 }
-"
-  )
+")
 
-
-(defun org-babel-dart-evaluate
-    (session body &optional result-type result-params)
-  "
-Evaluate BODY in external Dart process.
+(defun ob-dart-evaluate (session body &optional result-type result-params)
+  "Evaluate BODY in external Dart process.
 If RESULT-TYPE equals 'output then return standard output as a string.
 If RESULT-TYPE equals 'value then return the value of the last statement
 in BODY as elisp.
@@ -214,35 +202,31 @@ in BODY as elisp.
 Args:
   SESSION       - TODO DOCUMENT
   BODY          - String from org file, between #+begin_src and #+end_src
-                  - should be named: dart-src
-  RESULT-TYPE   - Symbol with value (output|value) that follows #+begin_src :results TODO DOCUMENT 
-                  - should be named: results-collection - Symbol - (output|value) -
+                - should be named: dart-src
+  RESULT-TYPE   - Babel :results param of (output|value)
+                - should be named: results-collection - Symbol - (output|value)
   RESULT-PARAMS - Symbol TODO DOCUMENT likely the 'format' type from docs
-                  - should be named: results-format
-"
-  (when session (error "Session is not (yet) supported for Dart."))
-  
+                - should be named: results-format"
+  (when session (error "Session is not (yet) supported for Dart"))
+
   (let* ((src-file (org-babel-temp-file "dart-"))
-         (wrapper (format org-babel-dart-wrapper-method body)))
-    
+         (wrapper (format ob-dart-wrapper-method body)))
+
     (with-temp-file src-file (insert wrapper))
     (let ((raw (org-babel-eval
-                (concat org-babel-dart-command " " src-file " " (symbol-name result-type)) "")))
+                (concat ob-dart-command " " src-file " " (symbol-name result-type)) "")))
       ;; result-type: both 'value and 'output formats results as table, unless raw is specified
       (org-babel-result-cond result-params
         raw
-        (org-babel-dart-table-or-string raw)))))
+        (ob-dart-table-or-string raw)))))
 
-(defun org-babel-prep-session:dart (session params)
+(defun org-babel-prep-session:dart (_session _params)
   "Prepare SESSION according to the header arguments specified in PARAMS."
-  (error "Session is not (yet) supported for Dart."))
+  (error "Session is not (yet) supported for Dart"))
 
-(defun org-babel-dart-initiate-session (&optional session)
-  "
-If there is not a current inferior-process-buffer in SESSION
-then create.  Return the initialized session.  Sessions are not
-supported in Dart.
-"
+(defun ob-dart-initiate-session (&optional _session)
+  "If there is not a current inferior-process-buffer in SESSION then create.
+Return the initialized session.  Sessions are not supported in Dart."
   nil)
 
 (provide 'ob-dart)
